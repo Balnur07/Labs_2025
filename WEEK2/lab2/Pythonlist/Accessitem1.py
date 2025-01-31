1bbdb06f421ac7b00499324472d0caa82c291031
Balnur07 <b_issabekova@kbtu.kz>
2025-01-31 16:59:24 +0000

    Add file lab2

---
+++ WEEK2/lab2/Pythonlist/Accessitem1.py
@@ -0,0 +1,17 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+#This will return the items from position 2 to 5.
+
+#Remember that the first item is position 0,
+#and note that the item in position 5 is NOT included
+
+mylist = ["apple", "banana", "cherry"]
+print(mylist[-1])
+
+thirdlist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thirdlist[-4:-1])
+
+fourthlist = ["apple", "banana", "cherry"]
+if "apple" in fourthlist:
+  print("Yes, 'apple' is in the fruits list")
